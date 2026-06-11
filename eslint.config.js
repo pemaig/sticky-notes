e6868f65b6010e65661ba001e670ec800d2de4b8
Maxim Pestriakov <maxim.pestriakov@sirenltd.com>
2026-06-11 15:35:22 +0200

    Add eslint, prettier, tailwind

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -15,8 +16,16 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
     languageOptions: {
       globals: globals.browser,
     },
   },
+  globalIgnores(['dist']),
+    prettierConfig,
 ])
